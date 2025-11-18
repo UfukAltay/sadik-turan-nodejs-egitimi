f75e94edb3bc427e4e60b50d7e5051dbc31a5071
UfukAltay <ufk.altay@gmail.com>
2025-11-18 13:27:26 +0300

    Bu derste basit bir HTTP sunucusu oluşturduk ve modülün ne olduğunu, http modülünün nasıl yüklendiğini ve kullanıldığını öğrendik.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("Hello, World! node.js");
+
+
+// Bu derste basit bir HTTP sunucusu oluşturduk ve modülün ne olduğunu, http modülünün nasıl yüklendiğini ve kullanıldığını öğrendik.
+var http = require("http"); // http modülünü yüklüyoruz
+
+function requestListener(request, response) {
+    response.end();
+}
+
+var server = http.createServer(requestListener);
+
+server.listen(8080); // sunucuyu 8080 portunda dinlemeye başlatıyoruz
+  
+console.log("Sunucu çalışıyor: http://localhost:8080");
