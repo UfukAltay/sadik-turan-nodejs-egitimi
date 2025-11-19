e75d76f0b06cfa0b6b751181ac2b5080e86425d8
UfukAltay <ufk.altay@gmail.com>
2025-11-19 18:51:49 +0300

    Routes Module konusu işlendi, yani kendi modülümüzü oluşturduk.

--- app.js
+++ app.js
@@ -1,67 +1,8 @@
-var http = require("http"); // http modülünü yüklüyoruz
-var fs = require("fs"); // dosya sistemi modülünü yüklüyoruz
+const http = require("http"); // http modülünü yüklüyoruz
+const routes = require("./routes"); // routes modülünü yüklüyoruz
 
 // ya da arrow function ile de tanımlayabiliriz
-var server = http.createServer((request, response) => {
-    
-    if (request.url == '/') {
-        fs.readFile("index.html", (err, data) => {
-        response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        response.write(data);            
-        response.end();
-        });
-    }
-    else if (request.url == '/blogs') {
-        fs.readFile("blogs.html", (err, data) => {
-        response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        response.write(data);            
-        response.end();
-        });
-    }
-    else if (request.url == '/create' && request.method == 'POST') {
-
-        const data = [];
-
-        request.on("data", (chunk) => {
-            data.push(chunk);
-            console.log("Gelen veri parçası: " + chunk);            
-        })
-
-        request.on("end", () => {
-            const result = Buffer.concat(data).toString();
-            const parsedData = result.split("=")[1].replace(/\+/g, ' ');
-            console.log("Tam veri: " + parsedData);                 
-
-            fs.appendFile("created_blogs.txt", result, (err) => {
-                if (err) {
-                    console.log("Dosyaya yazma hatası: " + err);                
-                }
-                else {
-                    response.statusCode = 302;
-                    response.setHeader('Location', '/blogs');
-                    response.end();                
-                }     
-            })       
-        });        
-
-
-    }
-    else if (request.url == '/create') {
-        fs.readFile("create.html", (err, data) => {
-        response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        response.write(data);            
-        response.end();
-        });
-    }    
-    else {
-        fs.readFile("404.html", (err, data) => {
-        response.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
-        response.write(data);            
-        response.end();
-        });
-    }
-});
-
-
+var server = http.createServer(routes);
 server.listen(3000);
+
 console.log("Sunucu çalışıyor: http://localhost:3000");
