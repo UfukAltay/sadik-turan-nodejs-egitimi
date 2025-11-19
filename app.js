8dae870ca93698a62e646fad290dc43031179c86
UfukAltay <ufk.altay@gmail.com>
2025-11-19 18:43:38 +0300

    Form Data konusu işlendi

--- app.js
+++ app.js
@@ -19,16 +19,32 @@ var server = http.createServer((request, response) => {
         });
     }
     else if (request.url == '/create' && request.method == 'POST') {
-        fs.appendFile("created_blogs.txt", "Yeni blog oluşturuldu.\n", (err) => {
-            if (err) {
-                throw err;                
-            }
-            else {
-                response.statusCode = 302;
-                response.setHeader('Location', '/blogs');
-                response.end();                
-            }            
+
+        const data = [];
+
+        request.on("data", (chunk) => {
+            data.push(chunk);
+            console.log("Gelen veri parçası: " + chunk);            
+        })
+
+        request.on("end", () => {
+            const result = Buffer.concat(data).toString();
+            const parsedData = result.split("=")[1].replace(/\+/g, ' ');
+            console.log("Tam veri: " + parsedData);                 
+
+            fs.appendFile("created_blogs.txt", result, (err) => {
+                if (err) {
+                    console.log("Dosyaya yazma hatası: " + err);                
+                }
+                else {
+                    response.statusCode = 302;
+                    response.setHeader('Location', '/blogs');
+                    response.end();                
+                }     
+            })       
         });        
+
+
     }
     else if (request.url == '/create') {
         fs.readFile("create.html", (err, data) => {
